817a2fdf91951019f6832122d41e2e881f142f57
Sonali Parekh <sonali.parekh912@gmail.com>
2023-04-07 21:43:48 +0530

    chore: setup mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { JournalController, JournalService } from '@journal';
 
 @Module({
   controllers: [JournalController],
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DB),
+  ],
   providers: [JournalService],
 })
 export class AppModule {}
