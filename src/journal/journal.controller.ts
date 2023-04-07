42754317d4e284e4d6b026397b80a764bddc69ef
Sonali Parekh <sonali.parekh912@gmail.com>
2023-04-07 23:27:40 +0530

    chore: create CRUD apis for journal

--- src/journal/journal.controller.ts
+++ src/journal/journal.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 import { CreateJournalDto } from './dto/createJournal.dto';
 import { JournalService } from './journal.service';
@@ -8,15 +16,62 @@ import { JournalDocument } from './schema/journal.schema';
 export class JournalController {
   constructor(private readonly journalService: JournalService) {}
 
-  @Get()
-  getHello(): string {
-    return this.journalService.getHello();
-  }
-
+  /**
+   * Creates a new journal.
+   * @param createJournalDto The data to create a journal.
+   * @param createJournalDto.title The title of the journal.
+   * @param createJournalDto.description (optional) The description of the journal.
+   * @returns The created journal.
+   */
   @Post('/create')
   async createJournal(
     @Body() createJournalDto: CreateJournalDto,
   ): Promise<JournalDocument> {
     return this.journalService.createJournal(createJournalDto);
+  }
+
+  /**
+   * Fetches all journals from the database.
+   * @returns List of all journals.
+   */
+  @Get('/all')
+  async getAllJournals(): Promise<JournalDocument[]> {
+    return this.journalService.getAllJournals();
+  }
+
+  /**
+   * Fetches a journal with the given id.
+   * @param id The id of the journal to fetch.
+   * @returns The journal with the given id.
+   */
+  @Get('/:id')
+  async getJournal(@Param('id') id: string): Promise<JournalDocument> {
+    return this.journalService.getJournal(id);
+  }
+
+  /**
+   * Updates a journal with the given id.
+   * @param id The id of the journal to update.
+   * @param createJournalDto The data to update the journal with.
+   * @param createJournalDto.title (optional) The title of the journal.
+   * @param createJournalDto.description (optional) The description of the journal.
+   * @returns
+   */
+  @Patch('/:id/update')
+  async updateJournal(
+    @Param('id') id: string,
+    @Body() createJournalDto: Partial<CreateJournalDto>,
+  ): Promise<JournalDocument> {
+    return this.journalService.updateJournal(id, createJournalDto);
+  }
+
+  /**
+   * Deletes a journal with the given id.
+   * @param id The id of the journal to delete.
+   * @returns The deleted journal.
+   */
+  @Delete('/:id/delete')
+  async deleteJournal(@Param('id') id: string): Promise<JournalDocument> {
+    return this.journalService.deleteJournal(id);
   }
 }
--- src/journal/journal.service.ts
+++ src/journal/journal.service.ts
@@ -11,11 +11,54 @@ export class JournalService {
     @InjectModel(Journal.name) private journalModel: Model<Journal>,
   ) {}
 
-  getHello(): string {
-    return 'Hello World!';
-  }
-
+  /**
+   * Creates a new journal.
+   * @param createJournalDto The data to create a journal.
+   * @param createJournalDto.title The title of the journal.
+   * @param createJournalDto.description (optional) The description of the journal.
+   * @returns The created journal.
+   */
   async createJournal(createJournalDto: CreateJournalDto) {
     return this.journalModel.create(createJournalDto);
+  }
+
+  /**
+   * Fetches all journals from the database.
+   * @returns List of all journals.
+   */
+  async getAllJournals() {
+    return this.journalModel.find().exec();
+  }
+
+  /**
+   * Fetches a journal with the given id.
+   * @param id The id of the journal to fetch.
+   * @returns THe journal with the given id.
+   */
+  async getJournal(id: string) {
+    return this.journalModel.findById(id);
+  }
+
+  /**
+   * Updates a journal with the given id.
+   * @param id The id of the journal to update.
+   * @param createJournalDto The data to update the journal with.
+   * @param createJournalDto.title (optional) The title of the journal.
+   * @param createJournalDto.description (optional) The description of the journal.
+   * @returns The updated journal.
+   */
+  async updateJournal(id: string, createJournalDto: Partial<CreateJournalDto>) {
+    return this.journalModel.findByIdAndUpdate(id, createJournalDto, {
+      new: true,
+    });
+  }
+
+  /**
+   * Deletes a journal with the given id.
+   * @param id The id of the journal to delete.
+   * @returns The deleted journal.
+   */
+  async deleteJournal(id: string) {
+    return this.journalModel.findByIdAndDelete(id);
   }
 }
