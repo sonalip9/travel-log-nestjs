a102822de410169731696acde4fb2b1c4bce5056
Sonali Parekh <sonali.parekh912@gmail.com>
2023-04-07 17:25:53 +0530

    chore(dev): setup dev tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,14 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import-alias'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
   ],
   root: true,
   env: {
@@ -21,5 +22,84 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    // General
+    'array-callback-return': 'warn',
+    'capitalized-comments': [
+      'error',
+      'always',
+      { line: { ignoreConsecutiveComments: true } },
+    ],
+    'default-param-last': 'error',
+    'id-length': ['warn', { properties: 'never', exceptionPatterns: ['^_'] }],
+    'max-lines': [
+      'error',
+      { max: 250, skipBlankLines: true, skipComments: true },
+    ],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-multi-assign': 'error',
+    'no-multi-spaces': 'warn',
+    'no-negated-condition': 'off',
+    'no-nested-ternary': 'off',
+    'no-prototype-builtins': 'warn',
+    'no-restricted-imports': 'warn',
+    'no-shadow': 'off',
+    'no-underscore-dangle': ['warn', { allow: ['_id'] }],
+    'no-var': 'error',
+    'object-shorthand': 'warn',
+    'one-var': ['error', 'never'],
+    'prefer-const': 'error',
+    'prefer-destructuring': 'error',
+    'prefer-object-spread': 'warn',
+    'sort-keys': ['error', 'asc', { caseSensitive: false, natural: true }],
+    'sort-vars': 'error',
+
+    // Import Plugin - rules available via `eslint-plugin-import`.
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+
+    // Typescript Plugin - rules available via `@typescript-eslint`.
+    '@typescript-eslint/no-shadow': ['error'],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': [
+      'error',
+      { allow: ['functions', 'arrowFunctions', 'methods'] },
+    ],
+
+    // Import alias Plugin - rules available via `eslint-plugin-import-alias`.
+    'import-alias/import-alias': [
+      'error',
+      {
+        relativeDepth: 2,
+        aliases: [
+          { alias: '@journal', matcher: 'src/journal' },
+          { alias: '@', matcher: './src' },
+        ],
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@journal', './src/journal'],
+          ['@', './src'],
+        ],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+    'import/external-module-folders': ['node_modules'],
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
   },
 };
