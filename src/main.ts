29a5738b05f95ba5e75a83038a4442c09c02cd65
Sonali Parekh <sonali.parekh912@gmail.com>
2023-04-21 18:50:33 +0530

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
@@ -17,7 +17,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
